a0677f3f8a4fd0ee0bf372a527115b625ece7ff6
jasonLapina <lapina.jason@gmail.com>
2022-10-07 11:12:11 +0800

    finish side-bar navigation

--- src/components/side-bar/SideBar.js
+++ src/components/side-bar/SideBar.js
@@ -1,6 +1,36 @@
 import classes from './SideBar.module.scss';
-const SideBar = (props) => {
-  return <nav className={`${classes.nav} ${props.className}`}>navigation</nav>;
+const SideBar = () => {
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        <li>
+          <div className={classes.icon}>
+            <ion-icon name='bed'></ion-icon>
+          </div>
+          <span>hotel</span>
+        </li>
+        <li>
+          <div className={classes.icon}>
+            <ion-icon name='airplane'></ion-icon>
+          </div>
+          <span>flight</span>
+        </li>
+        <li>
+          <div className={classes.icon}>
+            <ion-icon name='car-sport'></ion-icon>
+          </div>
+          <span>car rental</span>
+        </li>
+        <li>
+          <div className={classes.icon}>
+            <ion-icon name='map-outline'></ion-icon>
+          </div>
+          <span>tours</span>
+        </li>
+      </ul>
+      <p className={classes.legal}>© 2022 by Rasurv. All rights RASURVED.</p>
+    </nav>
+  );
 };
 
 export default SideBar;
